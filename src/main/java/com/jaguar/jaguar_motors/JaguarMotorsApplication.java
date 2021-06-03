0d8a3145d9ddb7ded942af392d67ac9082ea66e0
Bruno da Silva <brunodasilva@Mac-mini-de-Bruno.local>
2021-06-03 00:28:39 -0300

    start project

---
+++ src/main/java/com/jaguar/jaguar_motors/JaguarMotorsApplication.java
@@ -0,0 +1,14 @@
+package com.jaguar.jaguar_motors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaguarMotorsApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(JaguarMotorsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jaguar/jaguar_motors/JaguarMotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaguar.jaguar_motors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaguarMotorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
